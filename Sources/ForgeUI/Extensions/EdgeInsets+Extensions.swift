0171fcff842adcc54c147c173891b1988a802f3f
Dino Catalinac <dino.chata@gmail.com>
2026-07-31 09:31:26 +0200

    Add convenience insets initializer

---
+++ Sources/ForgeUI/Extensions/EdgeInsets+Extensions.swift
@@ -0,0 +1,10 @@
+// Created by Dino Catalinac on 31.07.2026.
+
+import SwiftUI
+
+public extension EdgeInsets {
+
+    init(_ insets: CGFloat) {
+        self.init(top: insets, leading: insets, bottom: insets, trailing: insets)
+    }
+}
