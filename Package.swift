ffff873e81bb034c772042cf16fa3c9a3277f681
Dino Catalinac <dino.chata@gmail.com>
2026-07-31 17:26:34 +0200

    Add Flow package

--- Package.swift
+++ Package.swift
@@ -30,7 +30,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
+        .package(url: "https://github.com/tevelee/SwiftUI-Flow", from: "3.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -66,7 +67,8 @@ let package = Package(
         .target(
             name: "ForgeUI",
             dependencies: [
-                "ForgeCore"
+                "ForgeCore",
+                .product(name: "Flow", package: "https://github.com/tevelee/SwiftUI-Flow")
             ],
             swiftSettings: [
                 .enableUpcomingFeature("ApproachableConcurrency"),
